b253aeea2ff4402e016fc28fbb71aacca7ebda80
Raki Saionji <rakisaionji@gmail.com>
2021-02-12 10:16:09 -0700

    Add gcc build support for assert.h

--- assert.h
+++ assert.h
@@ -1,8 +1,6 @@
 //
 // assert.h
 //
-//      Copyright (c) Microsoft Corporation. All rights reserved.
-//
 // Defines the assert macro and related functionality.
 //
 
@@ -11,6 +9,38 @@
 #error assert.h not for CRT internal use
 
 #endif
+
+#ifdef __GNUC__
+
+#include <crtdefs.h>
+
+#undef assert
+
+#if (defined _ISOC11_SOURCE || (defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L))
+
+#undef static_assert
+
+#define static_assert _Static_assert
+
+#endif
+
+#ifdef NDEBUG
+
+#define assert(expression) ((void)0)
+
+#else
+
+extern void __cdecl _assert(
+    char const* _Message,
+    char const* _File,
+    unsigned _Line
+);
+
+#define assert(expression) (void)((!!(expression)) || (_assert(#expression, __FILE__, (unsigned)(__LINE__)), 0))
+
+#endif
+
+#else /* MSVC */
 
 #include <corecrt.h>
 
@@ -35,3 +65,5 @@ _ACRTIMP void __cdecl _assert(
 #endif
 
 _CRT_END_C_HEADER
+
+#endif
